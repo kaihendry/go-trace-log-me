e1a6d02b2aa12deef433dc20343fe78dd43f78e2
Kai Hendry <hendry@iki.fi>
2021-08-17 09:29:57 +0800

    Mark service B

--- b/main.go
+++ b/main.go
@@ -23,7 +23,7 @@ func main() {
 
 		// log using ctx so we have the traceID to search with
 		// TODO: shouldn't http req path et al be logged here too?
-		ctx := log.WithFields(log.Fields{"traceID": traceID})
+		ctx := log.WithFields(log.Fields{"traceID": traceID, "service": "b"})
 
 		if traceID == "" {
 			ctx.Error("missing trace ID")
